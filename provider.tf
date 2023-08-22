d3e6afb1951be6c96008177b6cd5268846d5d685
NakulT3455 <125851295+NakulT3455@users.noreply.github.com>
2023-08-22 15:13:33 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -2,6 +2,8 @@ provider "aws" {
   region  = "ap-south-1" # Don't change the region
 }
 
+
+terraform {
 backend "s3" {
     bucket         	   = "3.devops.candidate.exam"
     key              	   = "nakul.thorat"
@@ -9,4 +11,5 @@ backend "s3" {
     #encrypt        	   = true
     #dynamodb_table = "mycomponents_tf_lockid"
   }
+}
 # Add your S3 backend configuration here
