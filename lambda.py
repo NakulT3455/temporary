437cdc3c6b65b1c1471569dc61f39d6dc649969c
NakulT3455 <125851295+NakulT3455@users.noreply.github.com>
2023-08-22 13:19:50 +0530

    Add files via upload

---
+++ lambda.py
@@ -0,0 +1,43 @@
+import requests
+import os
+import json
+import boto3
+import base64
+
+def lambda_handler(event, context):
+
+
+  payload = event
+  request_body = {
+        "name": "Nakul Thorat",
+        "subnet": ,
+        "emailID": "thoratnakul@gmail.com",
+        
+    }
+ 
+  request_headers = {
+        "X-Siemens-Auth": "test"
+    }
+
+  response = requests.post('https://ij92qpvpma.execute-api.eu-west-1.amazonaws.com/candidate-email_serverless_lambda_stage/data',
+                             headers=request_headers,
+                             data=json.dumps(request_body))
+
+  
+  if response.status_code == 201:
+    print(response.status_code)
+    
+    return {
+      'statusCode': 201,
+      'body': json.dumps({'message': 'Request sent successfully.'}),
+    }
+  else:
+    print(response.status_code)
+    
+    return {
+      'statusCode': response.status_code,
+      'body': json.dumps({'message': 'The request failed.'}),
+    }
+
+
+
