fc6e7c5141f107c8d6b8b0fdb3c4381f785c5893
NakulT3455 <125851295+NakulT3455@users.noreply.github.com>
2023-08-23 11:07:06 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,7 +1,7 @@
 provider "aws" {
   region  = "ap-south-1"
-  access_key = AKIAW7G5VF4TPTIUPKOL
-  secret_key  = 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
+  access_key = "AKIAW7G5VF4TPTIUPKOL"
+  secret_key  = "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"
 }
 
 
